9ae735ed67ea0e5d121920ca89400eec662c0a5e
k3cman <kecman.dev@gmail.com>
2021-10-12 10:08:08 +0200

    Initiate Blogs page

---
+++ src/pages/blogs.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import Layot from "../components/layout/Layout";
+import Footer from "../components/layout/Footer";
+
+const BlogsPage = () => {
+  return (
+    <Layot>
+      <main>Blog section</main>
+      <Footer />
+    </Layot>
+  );
+};
+
+export default BlogsPage;

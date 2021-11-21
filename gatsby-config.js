c6785b41f65969bbff91e59e78f43fb1117155b6
k3cman <kecman.dev@gmail.com>
2021-11-21 15:11:27 +0100

    Create default template for mdx plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,29 @@ module.exports = {
     title: "kecman.dev",
   },
   plugins: [
-    `gatsby-plugin-postcss`
+    `gatsby-plugin-postcss`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts/`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-page-creator",
+      options: {
+        path: `${__dirname}/src/posts`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        defaultLayouts: {
+          default: require.resolve(
+            "./src/components/templates/post-template.js"
+          ),
+        },
+      },
+    },
   ],
 };
